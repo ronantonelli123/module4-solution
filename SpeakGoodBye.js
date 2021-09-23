ea45afd22e904fbbed66a14044adaa3b7146be4f
Ron Antonelli <ronantonelli123@verizon.net>
2021-09-22 21:22:26 -0400

    mod4solution

---
+++ SpeakGoodBye.js
@@ -0,0 +1,16 @@
+
+
+(function (window) {
+var byeSpeaker = {};
+
+
+var speakWord = "Good Bye";
+
+byeSpeaker.speak = function () {
+  console.log(speakWord + " " +byeSpeaker.name);
+
+}
+
+window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ SpeakHello.js
@@ -0,0 +1,18 @@
+
+
+(function (window) {
+
+var helloSpeaker = {};
+
+var speakWord = "Hello";
+
+
+helloSpeaker.speak = function () {
+
+    console.log(speakWord + " " + helloSpeaker.name);
+
+}
+
+window.helloSpeaker = helloSpeaker;
+
+})(window);
